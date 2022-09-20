3d031783f2fef461eb8b3dcb1299f96c25458960
Vlad <vladhalchyshak@gmail.com>
2022-09-20 16:10:53 +0300

    init

---
+++ pages/api/users/index.js
@@ -0,0 +1,35 @@
+import connectMongo from "../../../database/connection";
+import ua from "../../../database/locales/ua.json";
+import de from "../../../database/locales/ua.json";
+import en from "../../../database/locales/ua.json";
+
+export default async function handler(req, res) {
+  connectMongo().catch(() =>
+    res.status(405).json({ error: "Error in the Connection" })
+  );
+
+  // type of request
+  const {  method } = req;
+
+  switch (method) {
+    case "GET":
+      
+      res.status(201).json({ method, name: "GET Request", data: ua });
+      break;
+    case "POST":
+      res.status(200).json({ method, name: "POST Request" });
+      break;
+    case "PUT":
+      res.status(200).json({ method, name: "PUT Request" });
+      break;
+    case "DELETE":
+      res.status(200).json({ method, name: "DELETE Request" });
+      break;
+    default:
+      res.setHeader(
+        "Cache-Control",
+        "public, s-maxage=10, stale-while-revalidate=59"
+      );
+      break;
+  }
+}
